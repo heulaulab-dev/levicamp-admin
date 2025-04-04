1d793d7613fbde3ff9ef0b3e84158df9ad1f4afd
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2025-04-04 19:51:58 +0700

    fix: Update API base URL for environment consistency
    - Changed baseURL in axios instance from NEXT_PUBLIC_TEST_API_URL to NEXT_PUBLIC_API_URL for correct API endpoint usage.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from '@/hooks/auth/useAuth';
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_TEST_API_URL,
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
 	headers: { 'Content-Type': 'application/json' },
 });
 
